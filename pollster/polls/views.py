5381f7368720316be618a96fb27dd7decd7d1387
EmL01 <lahdoemile7670@gmail.com>
2022-02-21 14:37:52 +0100

    fetch from db and display

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -1,6 +1,44 @@
-from django.shortcuts import render
+from django.http import Http404, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
 from .models import Question, Choice
 
 def index(request):
-    return render(request, 'polls/index.html')
+    questions = Question.objects.order_by('-created')
+    context = {
+        'questions': questions
+    }
+    return render(request, 'polls/index.html', context)
+
+def details(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist')
+    context = {
+        'question': question
+    }
+    return render(request, 'polls/details.html', context)
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        'question': question
+    }
+    return render(request, 'polls/results.html', context)
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        context = {
+            'question': question,
+            'error_message': 'You did not select a choice'
+        }
+        return render(request, 'polls/details.html', context)
+    else:
+        choice.votes += 1
+        choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
