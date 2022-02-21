00aa5d65b169cb41d1d6d65808c215530e30d3f9
EmL01 <lahdoemile7670@gmail.com>
2022-02-21 13:57:10 +0100

    db models + django admin

--- pollster/polls/admin.py
+++ pollster/polls/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Choice
+
+admin.site.site_header = 'Pollster Admin'
+admin.site.site_title = 'Pollster Admin'
+admin.site.index_title = 'Welcome to Pollster Admin'
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, { 'fields': ['text'] })
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
--- pollster/polls/models.py
+++ pollster/polls/models.py
@@ -1,3 +1,17 @@
+from tkinter import CASCADE
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    text = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.text
